12b5fa1b12b08e28d7a439d0d3faa8376c0e64d1
Seulki Lee <seulkilee94@gmail.com>
2023-12-03 13:32:03 +0900

    도메인 허용 삭제

--- static/js/form-submission-handler.js
+++ static/js/form-submission-handler.js
@@ -66,7 +66,7 @@
       var xhr = new XMLHttpRequest();
       xhr.open('POST', url);
       // xhr.withCredentials = true;
-      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded", "Access-Control-Allow-Origin:*");
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
       xhr.onreadystatechange = function() {
           if (xhr.readyState === 4 && xhr.status === 200) {
             form.reset();
